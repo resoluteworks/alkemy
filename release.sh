26f1920ab05669beacb1ca6f7b0d133c04915471
cosmin-marginean <cos@hey.com>
2023-04-22 19:54:21 +0100

    Cleanup

--- release.sh
+++ release.sh
@@ -2,11 +2,22 @@
 
 set -e
 
-./gradlew incrementSemanticVersion --patch
+git tag -l | xargs git tag -d
+git fetch --tags
+
 VERSION=`cat version.properties | grep "version" | awk -F' *= *' '{print $2}'`
 echo "Version is $VERSION"
 
-sed -i '' "s/alkemyVersion.*/alkemyVersion = $VERSION/g" sample-project/gradle.properties
-./gradlew clean build publish
-cd sample-project
-./gradlew clean test
+rm -rf docs/dokka
+./gradlew clean dokkaHtml
+./gradlew publish
+
+git add --all
+git commit -am "Release $VERSION"
+git push
+git tag "${VERSION}"
+git push --tags --force
+
+gh release create "${VERSION}" --verify-tag --title "Alkemy ${VERSION}" --notes "Version ${VERSION}"
+
+echo "Finished building version $VERSION"
